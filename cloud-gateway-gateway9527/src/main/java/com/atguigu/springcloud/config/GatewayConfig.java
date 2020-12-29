086751c26ce83788ba1f1f5551b95fabf0d3845e
meiko_glb <yishengxin_glb@163.com>
2020-12-29 21:14:43 +0800

    day20201229-commit01

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 配置了一个id为path_route_atguigu的路由规则
+     * 当访问地址http://localhost:9527/guonei时会转发到地址：http://news.baidu.com/guonei
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_atguigu",
+                        r -> r.path("/guonei")
+                            .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_baidu", r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
+
+        return routes.build();
+    }
+}
