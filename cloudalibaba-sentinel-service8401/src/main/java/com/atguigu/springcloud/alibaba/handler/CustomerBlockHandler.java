79e86e6f35f10591a0844c06d9ee484230cf3b85
yishengxin <957956791@qq.com>
2021-02-24 17:06:05 +0800

    springcloudalibaba sentinel

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/handler/CustomerBlockHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/handler/CustomerBlockHandler.java
@@ -2,7 +2,9 @@ package com.atguigu.springcloud.alibaba.handler;
 
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entities.CommonResult;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerBlockHandler {
 
     public static CommonResult handlerException(BlockException exception) {
