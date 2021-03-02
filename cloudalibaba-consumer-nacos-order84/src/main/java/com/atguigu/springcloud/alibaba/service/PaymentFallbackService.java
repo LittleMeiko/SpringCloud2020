9618f47be4d2c6c44557c51e5ad64805a7c3e157
yishengxin <957956791@qq.com>
2021-03-02 17:08:56 +0800

    springcloudalibaba-seata

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/alibaba/service/PaymentFallbackService.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/alibaba/service/PaymentFallbackService.java
@@ -4,16 +4,12 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import org.springframework.stereotype.Component;
 
-/**
- * @auther zzyy
- * @create 2020-02-25 18:30
- */
 @Component
 public class PaymentFallbackService implements PaymentService
 {
     @Override
-    public CommonResult<Payment> paymentSQL(Long id)
-    {
+    public CommonResult<Payment> paymentSQL(Long id) {
+
         return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
     }
 }
